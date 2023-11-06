50ab8afc347b9c4ed58305a8efab9c33f28ce3f2
AlejoMejia96 <alejandroquinteromejia@gmail.com>
2023-11-06 18:04:43 -0500

    HW web server finished

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -38,7 +38,7 @@ function App() {
   }, [access]);
 
   const onSearch = (id) => {
-    axios(`https://rym2.up.railway.app/api/character/${id}?key=${APIKEY}`)
+    axios(`http://localhost:3001/rickandmorty/character/${id}`)
       .then(({ data }) => {
         if (data.name) {
           setCharacters((oldCharacters) => [...oldCharacters, data]);
--- front/src/components/Detail/Detail.jsx
+++ front/src/components/Detail/Detail.jsx
@@ -10,7 +10,7 @@ function Detail() {
   const [character, setCharacter] = useState({});
 
   useEffect(() => {
-    axios(`https://rym2.up.railway.app/api/character/${id}?key=${APIKEY}`).then(
+    axios(`http://localhost:3001/rickandmorty/character/${id}`).then(
       ({ data }) => {
         if (data.name) {
           setCharacter(data);
